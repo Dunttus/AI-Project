845bf60fe3d93af0194dab6423aeea0e11aa1c5c
tqre <tkuure@gmail.com>
2020-02-03 20:55:51 +0200

    Made the script more user friendly and added UID check

--- EJK-stack/writelogtodatabase.sh
+++ EJK-stack/writelogtodatabase.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+set -e
+
+display_help() { 
+echo ''' 
+EJK-script usage:
+Helpful information coming your way soon(tm).
+'''
+}
+
+if [[ -z "$1" ]]; then display_help
+	echo ERROR: No arguments specified 
+	exit
+fi
+
+check_user() {
+	if [ $(id -u) != 0 ]; then
+		echo For full logs, run the script as root
+	fi
+}
+
 while read line; do
 	curl -XPOST localhost:9200/$1/_doc/?filter_path=_seq_no \
 	-H 'Content-type:application/json' \
