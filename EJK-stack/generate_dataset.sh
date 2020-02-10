1a7248fb804beae058406c552ac3a478d5e4122d
tqre <tkuure@gmail.com>
2020-02-10 21:12:46 +0200

    Funny sed parser makes CSVs now.
    Order might vary, todo is in the file itself.

--- EJK-stack/generate_dataset.sh
+++ EJK-stack/generate_dataset.sh
@@ -5,4 +5,17 @@ sudo journalctl -o json --output-fields=PRIORITY,MESSAGE |
 sed -E 's/"__CURSOR":"[^"]*"//;
 	s/"_BOOT_ID":"[^"]*"//;
 	s/"__MONOTONIC_TIMESTAMP":"[^"]*"//;
-	s/"__REALTIME_TIMESTAMP":"[^"]*"//' > testdataset.json
+	s/"__REALTIME_TIMESTAMP":"[^"]*"//;
+# Beginning curly brackets and any number of commas after
+	s/^\{,*//;
+# Last curly bracket and any number of commas before
+	s/,*}$//;
+# Trim separator to only one comma
+	s/",*"/","/;
+# Parses priority to a mere number
+	s/"PRIORITY":"([0-7]?)"/\1/;
+# Remove more
+	s/"MESSAGE"://' > testdataset.json
+	
+# TODO: the order varies, make it constant (priority,"message")	
+
