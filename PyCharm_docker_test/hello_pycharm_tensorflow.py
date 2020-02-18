ecef6839e620dd9e7c5a8b142943eef7ddae6161
tqre <tkuure@gmail.com>
2020-02-18 11:08:02 +0200

    Added testing code from tensorflow.org

--- PyCharm_docker_test/hello_pycharm_tensorflow.py
+++ PyCharm_docker_test/hello_pycharm_tensorflow.py
@@ -4,3 +4,14 @@ print("Hello, PyCharm, Docker and Tensorflow!")
 
 print(f"TensorFlow: {tf.__version__}")
 tf.config.list_physical_devices('GPU')
+
+# Testing GPU tensor assignment
+# https://www.tensorflow.org/guide/gpu
+# You should see:
+# Executing op MatMul in device /job:localhost/replica:0/task:0/device:GPU:0
+
+tf.debugging.set_log_device_placement(True)
+a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+c = tf.matmul(a, b)
+print(c)
