5411c82ce2e0d64b6bfd511d9ae196111ad6c808
tqre <tkuure@gmail.com>
2020-03-12 23:27:00 +0200

    Short sequences come out

--- classifier/text_classifier.py
+++ classifier/text_classifier.py
@@ -1,8 +1,22 @@
+import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pandas as pd
 
+# Testing file
 FILE = "ubuntu_logs_tail.json"
+LOG_DATA = ['PRIORITY', 'MESSAGE']
+#FILE = "../datasets/loglevels/training_logs.json"
 df = pd.read_json(FILE, lines=True)
+# Testing data needs to be stripped, not needed on big file
+df = df[LOG_DATA]
 
-print(df)
+# Bring on the Tokenizer!
+tok = Tokenizer(num_words=100, oov_token="<OOV>")
+tok.fit_on_texts(df.MESSAGE)
+# Here we see some numbers get attention, do we want that?
+print(tok.word_index)
+print(tok.word_counts)
+seq = pad_sequences(tok.texts_to_sequences(df.MESSAGE),maxlen=None)
+print(seq)
