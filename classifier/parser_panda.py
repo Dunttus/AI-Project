4c80afe568078fdabd443e74eeb6df81cebe0c9b
tqre <tkuure@gmail.com>
2020-03-12 14:43:54 +0200

    Check for loglevel population count added

--- classifier/parser_panda.py
+++ classifier/parser_panda.py
@@ -6,21 +6,26 @@ FILES = ["ubuntu_logs.json", "archelk_logs.json", "upcloudarch3_logs.json"]
 TESTFILE = "ubuntu_logs_tail.json"
 LOG_DATA = ['PRIORITY', 'MESSAGE']
 
+# Convenience function to check initial value counts
 def count_lines_per_loglevel():
     for file in FILES:
         print("Reading file: " + file + "...")
         df = pd.read_json(DS_LOC + file, lines=True)
         print(df['PRIORITY'].value_counts())
 
-# Uncomment to count
-#count_lines_per_loglevel()
-
 # Looking at the data, we'll start small. Maximum of 3000 entries per level,
 # 1000 from each file, randomized if there are more than 1000 in the category.
 # We don't have data from zero level, just skipping it for now.
 
-df = pd.read_json(TESTFILE, lines=True)
+df = pd.read_json(DS_LOC + FILES[0], lines=True)
 
-for i in range(4,7):
+for i in range(1,8):
     test = df.loc[df['PRIORITY'] == i]
-    print(test[LOG_DATA])
+    print("Log level " + str(i) + ": ", end ='')
+    if test['PRIORITY'].size > 1000:
+        # These entries have to be reduced
+        print(">1000")
+    else:
+        # These entries go straight into the dataset
+        print("<1000")
+
