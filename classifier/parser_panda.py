e0497856587d28c7b611a7520e4aa8d4e823f5d1
tqre <tkuure@gmail.com>
2020-03-12 13:49:12 +0200

    Put counting code inside a function, dont want to run it every time

--- classifier/parser_panda.py
+++ classifier/parser_panda.py
@@ -5,9 +5,11 @@ DS_LOC = "../datasets/loglevels/"
 FILES = ["ubuntu_logs.json", "archelk_logs.json", "upcloudarch3_logs.json"]
 TESTFILE = "ubuntu_logs_tail.json"
 
-for file in FILES:
-    print("Reading file: " + file + "...")
-    df = pd.read_json(DS_LOC + file, lines=True)
-    print(df['PRIORITY'].value_counts())
+def count_lines_per_loglevel():
+    for file in FILES:
+        print("Reading file: " + file + "...")
+        df = pd.read_json(DS_LOC + file, lines=True)
+        print(df['PRIORITY'].value_counts())
 
+count_lines_per_loglevel()
 
