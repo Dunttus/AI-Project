0be802bf68754669a9c4506f1f5e2b41a539b36e
tqre <tkuure@gmail.com>
2020-03-12 17:04:14 +0200

    Dataset splitter added

--- classifier/multiclassifier.py
+++ classifier/multiclassifier.py
@@ -1,8 +1,12 @@
 # Multi-class classifier for determining kernel log level from log message
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
-# Dataset locations
-DS_LOC = "../datasets/loglevels"
-DS_FILE = "training_logs.json"
+DS_FILE = "../datasets/loglevels/training_logs.json"
+# This is the full set containing DataFrame object
+df = pd.read_json(DS_FILE, lines=True)
 
+TRAINING_SET, TEST_SET = train_test_split(df, test_size=0.2)
+print(f"Training set entries: {TRAINING_SET.size}")
+print(f"Testing set entries: {TEST_SET.size}")
