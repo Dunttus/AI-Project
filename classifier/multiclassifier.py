36a78cdf8ab0f9afacb8e56566e274d945823ebd
tqre <tkuure@gmail.com>
2020-03-12 17:43:18 +0200

    Vectorizer function added from scikit

--- classifier/multiclassifier.py
+++ classifier/multiclassifier.py
@@ -1,12 +1,21 @@
 # Multi-class classifier for determining kernel log level from log message
+# https://realpython.com/python-keras-text-classification/
 import tensorflow as tf
-from sklearn.model_selection import train_test_split
+
+from sklearn.model_selection import train_test_split as split
+from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 
 DS_FILE = "../datasets/loglevels/training_logs.json"
 # This is the full set containing DataFrame object
 df = pd.read_json(DS_FILE, lines=True)
+TRAINING_SET, TEST_SET = split(df, test_size=0.2)
 
-TRAINING_SET, TEST_SET = train_test_split(df, test_size=0.2)
 print(f"Training set entries: {TRAINING_SET.size}")
 print(f"Testing set entries: {TEST_SET.size}")
+
+# Vectorizing the entires without any preprocessing
+vectorizer = CountVectorizer()
+TEST_V = vectorizer.fit(TEST_SET['MESSAGE'])
+print(TEST_V.vocabulary_)
+
