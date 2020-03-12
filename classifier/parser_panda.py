980c0ebda10ad6f4bc0c18d70096079d99684a34
tqre <tkuure@gmail.com>
2020-03-12 13:46:20 +0200

    Parser Panda counts log lines now. Readme update.

--- classifier/parser_panda.py
+++ classifier/parser_panda.py
@@ -1,9 +1,13 @@
 # Generate an class-evened dataset for classification with pandas
 import pandas as pd
 
-DS_LOC = "https://raw.githubusercontent.com/Dunttus/AI-Project/master/datasets/loglevels/"
+DS_LOC = "../datasets/loglevels/"
 FILES = ["ubuntu_logs.json", "archelk_logs.json", "upcloudarch3_logs.json"]
 TESTFILE = "ubuntu_logs_tail.json"
 
-df = pd.read_json(TESTFILE, lines=True)
-print(df['PRIORITY'].value_counts())
+for file in FILES:
+    print("Reading file: " + file + "...")
+    df = pd.read_json(DS_LOC + file, lines=True)
+    print(df['PRIORITY'].value_counts())
+
+
