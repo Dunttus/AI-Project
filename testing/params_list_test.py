14e49b8994de5a7c9640e1b2fc8ca42706677bff
tqre <tkuure@gmail.com>
2020-03-15 16:28:21 +0200

    code saves nested dict as a json file

--- testing/params_list_test.py
+++ testing/params_list_test.py
@@ -1,18 +1,25 @@
 # JSON format example for model data saving
 import datetime as dt
+import json
 DATA = {
 "version" : "v0.1",
-"timestamp" : dt.datetime.now().isoformat(timespec='minutes'),
+"timestamp" : dt.datetime.now().isoformat(timespec='seconds'),
 "parameters": {
     "test_set_size" : 0.2,
-    "epochs" : 50
+    "epochs" : 5590
     },
 "scores": {
     "accuracy" : 90.0
     },
 }
 
+# Accessing items
 print(DATA)
 print(DATA['parameters'])
 print(DATA['version'])
 print(DATA['parameters']['epochs'])
+
+# Write a nested dict into json:
+with open("data_test.json","a") as f:
+    json.dump(DATA, f)
+    f.write('\n')
