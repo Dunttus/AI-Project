6c86e7cfe0e73475574a4d8b7b5325c3381df650
tqre <tkuure@gmail.com>
2020-03-16 15:01:02 +0200

    Actually working replacement function
    Does nothing for now

--- classifier/ascii_decoder.py
+++ classifier/ascii_decoder.py
@@ -6,11 +6,10 @@ FILE = "decode_this.json"
 LOG_DATA = ['PRIORITY', 'MESSAGE']
 
 def test_function(x):
-    x = "test"
     return x
 
 df = pd.read_json(DS_LOC + FILE, lines=True)
 df = (df[LOG_DATA])
 print(df)
-df['MESSAGE'] = df.apply(test_function, axis=1)
+df.MESSAGE = df.MESSAGE.apply(test_function)
 print(df)
