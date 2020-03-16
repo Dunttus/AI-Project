f3d178344a0023e720b80ee39de7fc71f0f05fa5
tqre <tkuure@gmail.com>
2020-03-16 15:04:48 +0200

    function checks for datatype, reacts if it's a list

--- classifier/ascii_decoder.py
+++ classifier/ascii_decoder.py
@@ -6,6 +6,10 @@ FILE = "decode_this.json"
 LOG_DATA = ['PRIORITY', 'MESSAGE']
 
 def test_function(x):
+
+    if isinstance(x, list):
+        print("list detected, do conversion!")
+
     return x
 
 df = pd.read_json(DS_LOC + FILE, lines=True)
