13a341f2fb34a317e9f93641a83a934e1a38877c
tqre <tkuure@gmail.com>
2020-03-16 15:38:29 +0200

    Working converter

--- classifier/ascii_decoder.py
+++ classifier/ascii_decoder.py
@@ -5,20 +5,24 @@ FILE = "decode_this.json"
 
 LOG_DATA = ['PRIORITY', 'MESSAGE']
 
-def test_function(x):
+def datatype_check(data):
 
-    if isinstance(x, str):
-        print(type(x))
-        print("Everything is fine!")
-        return x
+    # We want all lines to be of type string
+    if isinstance(data, str):
+        return data
 
     else:
-        print(type(x))
-        print("Conversion needed")
-        return x
+        # UTF-8 fails here, invalid continuation byte
+        if isinstance(data, list):
+            data = bytes(data).decode("latin-1")
+            return data
+
+        else:
+            print("ERROR: Format could not be converted: ", type(data))
+            exit(1)
 
 df = pd.read_json(DS_LOC + FILE, lines=True)
 df = (df[LOG_DATA])
-print(df)
-df.MESSAGE = df.MESSAGE.apply(test_function)
+df.MESSAGE = df.MESSAGE.apply(datatype_check)
+
 print(df)
