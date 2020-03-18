ceafe31725719244abc46600808442d6660416c0
tqre <tkuure@gmail.com>
2020-03-18 11:07:43 +0200

    Code for demo, tokenizer file saved for reuse
    ...although the tokenizer file gets recreated every time main.py is run

--- Lokari/demo.py
+++ Lokari/demo.py
@@ -1,18 +1,17 @@
 from Lokari.nlp import load_tokenizer
-
+import pandas as pd
 
 def run(model):
     DEMO_FILE = '../datasets/loglevels/demologs.json'
 
-
-    # Messages we want to know log levels:
-
-
+    # Read the demo data
+    data = pd.read_json(DEMO_FILE, lines=True)
+    messages = data.MESSAGE
     #
     tokenizer = load_tokenizer()
+    tokenized_msgs = tokenizer.texts_to_matrix(messages, mode='tfidf')
 
-
-    #model.predict
-    print("Hello, this is demo code")
+    pred = model.predict(tokenized_msgs)
+    print(pred)
 
     return
