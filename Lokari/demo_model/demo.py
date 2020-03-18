dd70684aa4e025088297a8858baabbd21a5acacc
tqre <tkuure@gmail.com>
2020-03-18 13:14:36 +0200

    Demo package is now independent from Lokari-code

--- Lokari/demo_model/demo.py
+++ Lokari/demo_model/demo.py
@@ -2,29 +2,34 @@ from os import environ as env
 # Reduce Tensorflow output to console
 env['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-from Lokari.nlp import load_tokenizer
 from tensorflow.keras.utils import to_categorical as onehotencode
 from tensorflow.keras.models import load_model
 from sklearn.metrics import accuracy_score
-import pandas as pd
+import pandas
 import numpy as np
+import pickle
 
 def run(model):
-    DEMO_FILE = 'demologs.json'
 
-    # Read the demo data
-    data = pd.read_json(DEMO_FILE, lines=True)
+    # Load test data and tokenizer
+    data = pandas.read_json('demologs.json', lines=True)
+    with open('tfidf_tokenizer.pickle', 'rb') as file:
+        tokenizer = pickle.load(file)
+
+    # Separate loglevels from messages
     messages = data.MESSAGE
     real_loglevel = data.PRIORITY
-    # Load tokenizer & convert data to tfidf tokens
-    tokenizer = load_tokenizer()
+
+    # Convert data to tfidf tokens
     tokenized_msgs = tokenizer.texts_to_matrix(messages, mode='tfidf')
+
     # Convert the correct answers into one-hot-encoded arrays
     tokenized_answers = onehotencode(real_loglevel)
+
     # Feed the data into the model
     pred = model.predict(tokenized_msgs)
-    # Print the results in a nice format
 
+    # Print the results in a nice format
     np.set_printoptions(formatter={'float': '{: 0.3f}'.format})
 
     print("Message, distribution, predicted level, real level")
