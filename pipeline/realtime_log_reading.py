79c5b7cc16f32e5fefa73c92ee4608a93bebed12
tqre <tkuure@gmail.com>
2020-03-30 10:43:16 +0300

    Basic working functions, lots of room for improvement

--- pipeline/realtime_log_reading.py
+++ pipeline/realtime_log_reading.py
@@ -1,2 +1,22 @@
-# This code reads Apache web server access log in real time and feeds it
-# somewhere.
+# Using python generators to read a new line in a file
+
+
+def follow(file):
+
+    # Set the file object position to end
+    file.seek(0, 2)
+    while True:
+        line = file.readline()
+        if not line:
+            # sleep a bit? this is quite resource intensive atm
+            continue
+
+        yield line
+
+
+with open('testfile', 'r') as file:
+
+    for new_line in follow(file):
+        print(new_line, end='')
+
+
