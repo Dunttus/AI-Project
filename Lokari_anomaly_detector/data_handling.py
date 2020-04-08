d3a23d1edc30819b0fd5a2471425680aa730c03d
tqre <tkuure@gmail.com>
2020-04-08 18:03:30 +0300

    More data processing

--- Lokari_anomaly_detector/data_handling.py
+++ Lokari_anomaly_detector/data_handling.py
@@ -3,6 +3,7 @@ env['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences as pad
+from tensorflow.keras.utils import to_categorical as onehotencode
 from tensorflow.keras.layers import Input, Dense, concatenate
 from tensorflow.keras.models import Model
 import pandas as pd
@@ -23,12 +24,25 @@ print(df)
 # These are fed into a dual model, one which processes text, and the other
 # deals with numbers and categorical data.
 numdata = pd.DataFrame(columns=['status','byte','rtime','method'])
+numdata_processed = pd.DataFrame()
 textdata = pd.DataFrame()
 
 # time = timestamp OMITTED FOR NOW
 # ip = requesting ip address OMITTED FOR NOW
 # status = HTTP status code
-numdata['status'] = pd.Categorical(df['status'])
+# convert the status codes to strings for one-hot-encoding to work
+numdata['status'] = df['status'].astype(str)
+numdata['status'] = pd.Categorical(numdata['status'])
+numdata.status = numdata.status.cat.codes
+status_ohe = onehotencode(numdata.status)
+print(status_ohe)
+
+# TODO: onehotencode creates a numpy array, so combining with pandas dataframe
+# TODO: doesn't work out of the box.
+
+
+
+#print(testdf.shape)
 # byte = size of the requested object
 # int64 type not good for model.fit function
 numdata['byte'] = df['byte']
@@ -40,6 +54,7 @@ numdata['rtime'] = df['rtime']
 numdata['method'] = pd.Categorical(df['method'])
 numdata.method = numdata.method.cat.codes
 # url = first line of request
+
 # This is a char-based tokenizer, maxlen is the number of characters used
 # num_words=64 -is this enough for all characters in logs? test sample
 # has 53 characters...
@@ -53,8 +68,9 @@ textdata = pad(tok.texts_to_sequences(df['url']), maxlen=64, padding='post')
 
 print(numdata.dtypes)
 print(numdata)
+print("Final numeric data processed:")
+print(numdata_processed)
 print(textdata)
-print(df)
 
 # https://keras.io/getting-started/functional-api-guide/
 # https://keras.io/getting-started/sequential-model-guide/
@@ -67,8 +83,8 @@ num_input = Input(shape=numdata.shape[1], name='num_input')
 text_input = Input(shape=textdata.shape[1], name='text_input')
 
 # NLP branch:
-first_nlp = Dense(128, input_dim=64, activation='relu')(text_input)
-second_nlp = Dense(64, activation='relu')(first_nlp)
+first_nlp = Dense(64, activation='relu')(text_input)
+second_nlp = Dense(32, activation='relu')(first_nlp)
 nlp_output = Dense(8, activation='relu')(second_nlp)
 
 # Combining the branches with concatenate
@@ -76,6 +92,7 @@ nlp_output = Dense(8, activation='relu')(second_nlp)
 # Concatenation is done for 8 layers from nlp_output and 4 from num_input
 output = concatenate([num_input, nlp_output])
 output = Dense(12, activation='sigmoid')(output)
+output = Dense(1, activation='sigmoid')(output)
 model = Model(inputs=[num_input, text_input], outputs=[output])
 
 # Check: Loss function! Normalization in the autoencoding phase??
