78935ceb3bebff3b42df259e54f3b90cf33ab682
tqre <tkuure@gmail.com>
2020-04-08 16:47:11 +0300

    Notes, dual input model compiles, data formatting not correct yet

--- Lokari_anomaly_detector/data_handling.py
+++ Lokari_anomaly_detector/data_handling.py
@@ -3,7 +3,8 @@ env['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences as pad
-from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Input, Dense, concatenate
+from tensorflow.keras.models import Model
 import pandas as pd
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -29,21 +30,25 @@ textdata = pd.DataFrame()
 # status = HTTP status code
 numdata['status'] = pd.Categorical(df['status'])
 # byte = size of the requested object
+# int64 type not good for model.fit function
 numdata['byte'] = df['byte']
 # rtime = time taken to serve the request
+# int64 not good for model.fit
 numdata['rtime'] = df['rtime']
 # method
+# transform to categorical data? shows as int still
 numdata['method'] = pd.Categorical(df['method'])
 numdata.method = numdata.method.cat.codes
 # url = first line of request
 # This is a char-based tokenizer, maxlen is the number of characters used
 # num_words=64 -is this enough for all characters in logs? test sample
 # has 53 characters...
+# HTTP Request body does %<code> with exotic characters %20 = space
 tok = Tokenizer(num_words=64, filters='',
                 lower=False, split='',char_level=True)
 df['url'] = df['url'].astype(str)
 tok.fit_on_texts(df['url'])
-textdata = pad(tok.texts_to_sequences(df['url']), maxlen=80, padding='post')
+textdata = pad(tok.texts_to_sequences(df['url']), maxlen=64, padding='post')
 #protocol OMITTED FOR NOW
 
 print(numdata.dtypes)
@@ -51,8 +56,41 @@ print(numdata)
 print(textdata)
 print(df)
 
-# Define 2 different imputs and their sizes
+# https://keras.io/getting-started/functional-api-guide/
+# https://keras.io/getting-started/sequential-model-guide/
+# https://keras.io/models/model/
+
+# Define 2 different inputs and their sizes
 print(f"Numerical feature vector inputs: {numdata.shape[1]}")
 print(f"Text tokenizer vector inputs: {textdata.shape[1]}")
 num_input = Input(shape=numdata.shape[1], name='num_input')
 text_input = Input(shape=textdata.shape[1], name='text_input')
+
+# NLP branch:
+first_nlp = Dense(128, input_dim=64, activation='relu')(text_input)
+second_nlp = Dense(64, activation='relu')(first_nlp)
+nlp_output = Dense(8, activation='relu')(second_nlp)
+
+# Combining the branches with concatenate
+# Autoencoding layer: 8 + 4 inputs and outputs
+# Concatenation is done for 8 layers from nlp_output and 4 from num_input
+output = concatenate([num_input, nlp_output])
+output = Dense(12, activation='sigmoid')(output)
+model = Model(inputs=[num_input, text_input], outputs=[output])
+
+# Check: Loss function! Normalization in the autoencoding phase??
+model.compile(optimizer='adam', loss='mean_squared_error', metrics=None,
+              loss_weights=None, sample_weight_mode=None,
+              weighted_metrics=None, target_tensors=None)
+
+# All options
+# y value is the same as input?? Check this in depth
+#model.fit(x=[numdata, textdata],
+#          y=None,
+#          batch_size=None,
+#          epochs=10, verbose=1,
+#          callbacks=None, validation_split=0.0, validation_data=None,
+#          shuffle=True, class_weight=None, sample_weight=None,
+#          initial_epoch=0, steps_per_epoch=None, validation_steps=None,
+#          validation_freq=1, max_queue_size=10, workers=1,
+#          use_multiprocessing=False)
