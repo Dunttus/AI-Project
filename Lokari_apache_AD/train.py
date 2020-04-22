55d88f16ab6e81f731e16fa5be160dd16ace5c0e
tqre <tkuure@gmail.com>
2020-04-22 14:18:36 +0300

    Changed the file path to this directory

--- Lokari_apache_AD/train.py
+++ Lokari_apache_AD/train.py
@@ -5,7 +5,7 @@ from Lokari_apache_AD.read_data import read
 # Read training data
 # read_data.py is given the file
 # read_data.py returns pandas dataframe
-data = read('../datasets/apache_access_log/good_access.log')
+data = read('training_dataset/good_access.log')
 
 # Process training data
 # the dataframe is sliced and processed into numbers
