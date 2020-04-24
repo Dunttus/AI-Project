836ae964cfb6dc5db4852dad5b25cd14d8cf24a5
tqre <tkuure@gmail.com>
2020-04-24 15:51:44 +0300

    Data processing functions ready for testing

--- Lokari_apache_AD/process.py
+++ Lokari_apache_AD/process.py
@@ -1,7 +1,8 @@
 # Convert the log data into fully numerical presentation.
 # If the model is being trained, save all tokenizers used.
+import pandas
 from keras_preprocessing.text import Tokenizer
-from keras_preprocessing.sequence import pad_sequences as pad
+from tensorflow.keras.preprocessing.sequence import pad_sequences as pad
 
 
 def process_apache_log(data):
@@ -9,7 +10,7 @@ def process_apache_log(data):
     # ["time", "ip", "status", "byte", "rtime", "method", "url", "protocol"]
     # We are dropping some for now
     processed = data.drop(columns=['time','ip','protocol'])
-    print(processed.columns)
+    #print(processed.columns)
     # ['status', 'byte', 'rtime', 'method', 'url']
 
     processed.status = tokenize_http_status(data.status)
@@ -17,8 +18,9 @@ def process_apache_log(data):
     processed.rtime = normalize_response_time(data.rtime)
     processed.method = tokenize_http_methods(data.method)
     processed.url = tokenize_url(data.url)
-    print(processed)
 
+    #print(processed)
+    #print(processed.dtypes)
     return processed
 
 
@@ -33,14 +35,14 @@ def tokenize_http_status(data):
 
 def normalize_response_size(bytes):
 
-    # calculate average size and normalize? zscore?
+    # calculate average size and normalize?
 
     return bytes
 
 
 def normalize_response_time(time):
 
-    # calculate average size and normalize? zscore?
+    # calculate average size and normalize?
 
     return time
 
@@ -58,12 +60,12 @@ def tokenize_url(data):
 
     tokenizer = Tokenizer(num_words=128, char_level=True)
     tokenizer.fit_on_texts(data)
+    data = tokenizer.texts_to_sequences(data)
+    # Pad the data later to be of equal length?
+    # Keras pad_sequences changes the datatype to numpy.ndarray, which doesn't
+    # fit into a pandas column easy.
 
-    print(type(data))
-    # This doesn't work correctly yet. Have to put the whole sequence
-    # in to the url column.
-    data = pad(tokenizer.texts_to_sequences(data), maxlen=64,
-                   padding='post')
-    #textdata2 = tokenizer.texts_to_matrix(data, mode='tfidf')
+    # tfidf piece tokenizer for testing
+    #data = tokenizer.texts_to_matrix(data, mode='tfidf')
 
     return data
