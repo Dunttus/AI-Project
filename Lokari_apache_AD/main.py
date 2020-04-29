d61b66e7a5ad30347a1212e62a00aea123c1deaf
tqre <tkuure@gmail.com>
2020-04-29 12:11:03 +0300

    main.py ready to write prediction tests

--- Lokari_apache_AD/main.py
+++ Lokari_apache_AD/main.py
@@ -20,15 +20,15 @@ config.SAVE = False
 set_output()
 env['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# Read logs
-data = read('training_dataset/good_access.log')
+# Read data
+data = read('training_dataset/bad_access.log')
+
+# Load model
+model_file = 'saved_models/' + config.VERSION + \
+             '/Lokari-v' + config.VERSION + '.h5'
+model = load_model(model_file)
 
 # Process new log lines
 data, urldata = process_apache_log(data)
 
-# Load the trained model
-# TODO LATER: Load the model first as it takes some seconds
-# This is now done here for debugging the processing part
-#model_file = 'saved_models/' + config.VERSION + \
-#             '/Lokari-v' + config.VERSION + '.h5'
-#load_model(model_file)
+# Predict!
