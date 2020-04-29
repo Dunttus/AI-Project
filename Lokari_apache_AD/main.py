b7db06af5588feecaf45996ce77f09c20c7d569e
tqre <tkuure@gmail.com>
2020-04-29 09:10:30 +0300

    Shallower autoencoder, first fully saved and trained model 4 testing

--- Lokari_apache_AD/main.py
+++ Lokari_apache_AD/main.py
@@ -1,2 +1,7 @@
 # The main function to start monitor and use the trained model
 
+# In testing: testing with handcrafted bad_access.log
+
+# Load model and it's parameters
+
+from tensorflow.keras.models import load_model
