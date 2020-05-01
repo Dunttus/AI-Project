1079d68e476947533a792188ab8be25a7cec920e
tqre <tkuure@gmail.com>
2020-05-01 19:57:06 +0300

    Model scores are calculated for comparison now

--- Lokari_apache_AD/main.py
+++ Lokari_apache_AD/main.py
@@ -1,13 +1,7 @@
 # The main function to start monitor and use the trained model
-
-# In testing: testing with handcrafted bad_access.log
-
 # Lokari Apache log anomaly detector:
-# Train the baseline model
 from os import environ as env
-import numpy, pandas
 from tensorflow.keras.models import load_model
-from sklearn import metrics
 from Lokari_apache_AD.read_data import read
 from Lokari_apache_AD.output_opts import set_output
 from Lokari_apache_AD.process import process_apache_log
@@ -31,16 +25,21 @@ model = load_model(model_file)
 
 # We need to know the baseline error scores to be able to compare them
 # with new incoming data. This part needs to be run only once.
-
-# TODO: make a function to calculate MSEs from the data
-# TODO: make sense of url MSE
-# NOTE: url MSE might not be needed for basic functionality, as URL data
-# affects the inputs, outputs and the training process anyway. Test it.
 # NOTE: the original dataset is needed here!
-# TODO: This could be done in the training process!
+# TODO: This really should be done in the training process!
+
+model_data = read('training_dataset/combine_access.log')
+m_data, m_url = process_apache_log(model_data)
+m_before_ae = [m_data.status, m_data.byte, m_data.rtime,
+               m_data.method, m_url]
+m_after_ae = model.predict(m_before_ae)
+print("Model Status MSE:", msescore(m_after_ae[0], m_before_ae[0]))
+print("Model Byte MSE:", msescore(m_after_ae[1], m_before_ae[1]))
+print("Model Rtime MSE:", msescore(m_after_ae[2], m_before_ae[2]))
+print("Model Method MSE:", msescore(m_after_ae[3], m_before_ae[3]))
 
 # The data that is fed to the model, can be multi-line
-incoming_data = read('training_dataset/single.log')
+incoming_data = read('training_dataset/bad_single.log')
 # Process new log line(s)
 data, url = process_apache_log(incoming_data)
 # The data that has not been through autoencoder yet
@@ -56,4 +55,4 @@ print("Status MSE:", msescore(after_ae[0], before_ae[0]))
 print("Byte MSE:", msescore(after_ae[1], before_ae[1]))
 print("Rtime MSE:", msescore(after_ae[2], before_ae[2]))
 print("Method MSE:", msescore(after_ae[3], before_ae[3]))
-
+# TODO: make sense of url MSE
