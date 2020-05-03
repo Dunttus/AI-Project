fffdbd6d81b45a7c580f75272aa0d3a2b938a68f
tqre <tkuure@gmail.com>
2020-05-03 17:25:42 +0300

    Cleaned code

--- Lokari_apache_AD/monitor.py
+++ Lokari_apache_AD/monitor.py
@@ -1,15 +1,13 @@
 # Lokari Apache log anomaly detector:
 # Set up a generator function to get new lines in a file
-
 from time import sleep
-
-FILE = 'testing_files/access.log'
 
 
 def follow(target):
 
     # Set the file object position to end
     target.seek(0, 2)
+
     while True:
         line = target.readline()
         if not line:
